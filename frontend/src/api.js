ffcfae1eaefe7a4ccc60f42c97e8d50a68520e68
subhoa4-creator <subhoa4@gmail.com>
2026-09-19 00:25:12 +0530

    Create api.js

---
+++ frontend/src/api.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+
+const api = axios.create({
+  baseURL: API_URL,
+  timeout: 60000,
+})
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token')
+      localStorage.removeItem('user')
+      if (window.location.pathname !== '/') {
+        window.location.href = '/'
+      }
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
+
+export const API_BASE = API_URL
